1323bf54d06e0a8c77e5fc10d485c85fa1b09c96
Dustin Chapman <dchapg@gmail.com>
2023-12-10 07:39:58 +0300

    working draft

--- whatIsBjj.js
+++ whatIsBjj.js
@@ -1,28 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", function(){
-	const text = "What is Brazilian Jiu-Jitsu?";
-	const textContainer = document.getElementById("handwriting-text");
-	const canvas = document.getElementById("animation-canvas");
-	const ctx = canvas.getContext("2d");
-	
-	//splits text into indiv characters and wraps each char in a span
-	const characters = text.split("");
-	const spannedText = characters.map(char => `<span>${char}</span>`).join("");
-	
-	textContainer.innerHTML = spannedText;
-	
-	//Animation
-	let xPos = 0;
-	
-	function draw() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.font = "24px Courier New";
-		ctx.fillText("What is Brazilian Jiu-Jitsu?", xPos, 50);
-		xPos += 2;
-		requestAnimationFrame(draw);
-	}
-	
-	draw();
-}); */
 
 document.addEventListener("DOMContentLoaded", function(){
     const text = "What is Brazilian Jiu-Jitsu?";
