c8614f019c1626bdf1a3a10d5ee9e1d371c1efe8
Dustin Chapman <dchapg@gmail.com>
2023-12-10 07:55:34 +0300

    remove unessary script for animation

--- whatIsBjj.js
+++ whatIsBjj.js
@@ -1,18 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function(){
-    const text = "What is Brazilian Jiu-Jitsu?";
-    const textContainer = document.getElementById("handwriting-text");
     const canvas = document.getElementById("animation-canvas");
-    const ctx = canvas.getContext("2d");
-
-    // Splits text into individual characters and wraps each character in a span
-    const characters = text.split("");
-    const spannedText = characters.map(char => `<span>${char}</span>`).join("");
-
-    textContainer.innerHTML = spannedText;
 
     // Animation
     let xPos = 0;
+    let startTime;
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
