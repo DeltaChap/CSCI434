7990fb57beecaba4de1aefeba35124eb4e79d185
Dustin Chapman <dchapg@gmail.com>
2023-12-12 09:00:45 +0300

    added pages

--- whatIsBjj.js
+++ whatIsBjj.js
@@ -1,19 +1,6 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-    const canvas = document.getElementById("animation-canvas");
-
-    // Animation
-    let xPos = 0;
-    let startTime;
-
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.font = "24px Courier New";
-        ctx.fillText("What is Brazilian Jiu-Jitsu?", xPos, 50);
-        xPos += 2;
-        requestAnimationFrame(draw);
-    }
-
-    // Initial call to start the animation
-    draw();
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    var definitionContainer = document.getElementById("definition-container");
+    definitionContainer.style.display = "block";
+  }, 3000); // 3000 milliseconds = 3 seconds
 });
